5b13153e035f22e370c702d83c8b2e0e7b997691
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-09-27 17:19:48 -0300

    Conditional format

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+interface CardProps {
+  produto: string;
+  valor: number;
+  desconto: number;
+  funcao: any;
+}
+
+export default function Card(props: CardProps) {
+  return (
+    <div className="card">
+      <div>Produto:{props.produto}</div>
+      <div>Valor: R$ {props.valor.toFixed(2)}</div>
+      {props.desconto > 0 && (
+        <div>Desconto: {(props.desconto * Math.floor(100)).toFixed(2)}%</div>
+      )}
+      <div>
+        Preço Venda: R$ {props.funcao(props.valor, props.desconto).toFixed(2)}
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Card from "@/components/Card";
+
+function calcDesc(n1: number, n2: number): number {
+  return n1 * (1 - n2);
+}
 
 export default function Home() {
   return (
@@ -7,7 +12,30 @@ export default function Home() {
       <Header />
       <main>
         <div className="main wrapper">
-          <div> HELLO</div>
+          <Card
+            produto={"Mouse"}
+            valor={49.9}
+            desconto={0.2}
+            funcao={calcDesc}
+          />
+          <Card
+            produto={"Teclado"}
+            valor={109.9}
+            desconto={0.1}
+            funcao={calcDesc}
+          />
+          <Card
+            produto={"Monitor"}
+            valor={749.9}
+            desconto={0.5}
+            funcao={calcDesc}
+          />
+          <Card
+            produto={"Cadeira"}
+            valor={349.0}
+            desconto={0.07}
+            funcao={calcDesc}
+          />
         </div>
       </main>
       <Footer />
